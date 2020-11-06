10dfd109199c68b269e48d2c8d12173a52ff6c60
cium123 <53029590+cium123@users.noreply.github.com>
2020-11-06 16:47:03 +0800

    Update simplelayout.py

--- simplelayout.py
+++ simplelayout.py
@@ -5,14 +5,14 @@ import os
 
 def main():
     parser = argparse.ArgumentParser(description='simplelayout')
-    parser.add_argument('--board_grid', help="Layout_panel_resolution", 
+    parser.add_argument('--board_grid', help="Layout_panel_resolution",
                         type=int)
-    parser.add_argument('--unit_grid', help="Rectangular_component_resolution", 
+    parser.add_argument('--unit_grid', help="Rectangular_component_resolution",
                         type=int)
     parser.add_argument('--unit_n', help="unit_of_Numbers", type=int)
-    parser.add_argument('--positions', type=int, nargs='+', 
+    parser.add_argument('--positions', type=int, nargs='+',
                         help="position_of_units")
-    parser.add_argument('--outdir', '--o', help="content_of_result", 
+    parser.add_argument('--outdir', '--o', help="content_of_result",
                         type=str)
     parser.add_argument('--file_name', help="name_of_file", type=str)
     args = parser.parse_args()
@@ -33,6 +33,6 @@ def main():
     with open(filename + '.jpg', 'w') as _:
         pass
 
-    
+
 if __name__ == "__main__":
     main()
