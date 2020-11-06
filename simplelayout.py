93f4b78b579fa1b6b03554d71e7d8a51db58853f
cium123 <53029590+cium123@users.noreply.github.com>
2020-11-06 15:46:42 +0800

    Update simplelayout.py

--- simplelayout.py
+++ simplelayout.py
@@ -1,9 +1,33 @@
-# import argparse
+import argparse
+import sys
+import os
 
 
 def main():
-    raise NotImplementedError  # TODO: 删除该行，并在下方实现代码
+    parser = argparse.ArgumentParser(description='simplelayout') 
+    parser.add_argument('--board_grid', help="Layout_panel_resolution", type=int)
+    parser.add_argument('--unit_grid', help="Rectangular_component_resolution", type=int)
+    parser.add_argument('--unit_n', help="unit_of_Numbers", type=int)
+    parser.add_argument('--positions', type=int,nargs='+', help="position_of_units")
+    parser.add_argument('--o', '--outdir', help="content_of_result", type=str)
+    parser.add_argument('--filename', help="name_of_file", type=str)
+    args = parser.parse_args()
 
+    if args.board_grid % args.unit_grid != 0:
+        sys.exit('User exit')
+    if args.unit_n != len(args.positions):
+        sys.exit('User exit')
+    if min(args.positions) < 1:
+        sys.exit('User exit')
+    if max(args.positions) > (args.board_grid / args.unit_grid)**2:
+        sys.exit('User exit')
+    if not os.path.exists(args.outdir):
+        os.makedirs(args.outdir)
+    filename = args.outdir + '/' + args.file_name
+    with open(filename + '.mat', 'w') as _:
+        pass
+    with open(filename + '.jpg', 'w') as _:
+        pass
 
 if __name__ == "__main__":
     main()
