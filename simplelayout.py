a6bef8a65f9c539e84699bd4d4834c43acb30a89
cium123 <53029590+cium123@users.noreply.github.com>
2020-11-06 16:27:45 +0800

    Update simplelayout.py

--- simplelayout.py
+++ simplelayout.py
@@ -8,9 +8,9 @@ def main():
     parser.add_argument('--board_grid', help="Layout_panel_resolution", type=int)
     parser.add_argument('--unit_grid', help="Rectangular_component_resolution", type=int)
     parser.add_argument('--unit_n', help="unit_of_Numbers", type=int)
-    parser.add_argument('--positions', type=int,nargs='+', help="position_of_units")
+    parser.add_argument('--positions', type=int, nargs='+', help="position_of_units")
     parser.add_argument('--o', '--outdir', help="content_of_result", type=str)
-    parser.add_argument('--filename', help="name_of_file", type=str)
+    parser.add_argument('--file_name', help="name_of_file", type=str)
     args = parser.parse_args()
 
     if args.board_grid % args.unit_grid != 0:
