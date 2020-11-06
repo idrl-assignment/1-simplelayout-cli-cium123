82c337750cc7fb94e7769a11f43a764677310c56
cium123 <53029590+cium123@users.noreply.github.com>
2020-11-06 16:43:24 +0800

    Update simplelayout.py

--- simplelayout.py
+++ simplelayout.py
@@ -4,12 +4,16 @@ import os
 
 
 def main():
-    parser = argparse.ArgumentParser(description='simplelayout') 
-    parser.add_argument('--board_grid', help="Layout_panel_resolution", type=int)
-    parser.add_argument('--unit_grid', help="Rectangular_component_resolution", type=int)
+    parser = argparse.ArgumentParser(description='simplelayout')
+    parser.add_argument('--board_grid', help="Layout_panel_resolution", 
+                        type=int)
+    parser.add_argument('--unit_grid', help="Rectangular_component_resolution", 
+                        type=int)
     parser.add_argument('--unit_n', help="unit_of_Numbers", type=int)
-    parser.add_argument('--positions', type=int, nargs='+', help="position_of_units")
-    parser.add_argument('--o', '--outdir', help="content_of_result", type=str)
+    parser.add_argument('--positions', type=int, nargs='+', 
+                        help="position_of_units")
+    parser.add_argument('--outdir', '--o', help="content_of_result", 
+                        type=str)
     parser.add_argument('--file_name', help="name_of_file", type=str)
     args = parser.parse_args()
 
@@ -17,7 +21,9 @@ def main():
         sys.exit('User exit')
     if args.unit_n != len(args.positions):
         sys.exit('User exit')
-    if min(args.positions) < 1 or max(args.positions) > (args.board_grid / args.unit_grid)**2:
+    if min(args.positions) < 1:
+        sys.exit('User exit')
+    if max(args.positions) > (args.board_grid / args.unit_grid)**2:
         sys.exit('User exit')
     if not os.path.exists(args.outdir):
         os.makedirs(args.outdir)
@@ -27,5 +33,6 @@ def main():
     with open(filename + '.jpg', 'w') as _:
         pass
 
+    
 if __name__ == "__main__":
     main()
